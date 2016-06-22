91f5842dd41baf1ca27cd9ec5c1d14d1bc1a4d08
Ilizaliturri-Albert <ajilizal@gmail.com>
2016-06-22 15:25:15 -0700

    Create ApplicationController.java

---
+++ Application-Controller/ApplicationController.java
@@ -0,0 +1,31 @@
+public class ApplicationController {
+	
+	private Dispatcher dispatcher;
+	
+	public ApplicationController() {
+		
+		dispatcher = new Dispatcher();
+		
+	}
+	
+	private boolean isAuthenticUser() {
+		
+		System.out.println("User is authenticated successfully.");
+		return true;
+	}
+	
+	private void trackRequest(String request) {
+		
+		System.out.println("Page Request: " + request);
+		
+	}
+	
+	public void dispatchRequest( String request ) {
+		
+		trackRequest(request);
+		
+		if(isAuthenticUser()) {
+			dispatcher.dispatch(request);
+		}
+	}
+}
